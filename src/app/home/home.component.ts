cb4bb2ed14245af4d9e0af857db2f769d86d6e5d
lucas melo <lucasmeloil@pm.me>
2024-01-22 20:26:22 -0300

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,24 +13,27 @@ export class HomeComponent {
   weight: number | undefined;
   height: number | undefined;
   result: any;
-  errorMessage: string = '';
+  errorMessage: string; // Deixe a inicialização para o construtor
 
-  constructor(private router: Router, private toastr: ToastrService) {}
+  constructor(private router: Router, private toastr: ToastrService) {
+    this.errorMessage = ''; // Inicialize a propriedade no construtor
+  }
 
   calculateIMC() {
     // Validar se os campos estão preenchidos
-    if (this.patientName && this.weight && this.height) {
+    if (this.patientName && this.weight !== undefined && this.height !== undefined) {
       // Lógica de cálculo do IMC
       const result = this.weight / (this.height * this.height);
 
       // Exibir mensagem de sucesso
-      this.toastr.success('Consulta do IMC realizada com sucesso!');
+      this.toastr.success('Cálculo do IMC realizado com sucesso!');
 
       // Redireciona para a página de resultados, passando o nome do paciente
       this.router.navigate(['/result'], { queryParams: { result, patientName: this.patientName } });
     } else {
-      // Mensagem de erro customizada para exibir YUYT5na tela
+      // Utiliza ngx-toastr para exibir mensagem de erro
       this.errorMessage = 'Por favor, preencha todos os campos.';
+      this.toastr.error(this.errorMessage, 'Erro');
     }
   }
 }
