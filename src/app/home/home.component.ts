9380a3763e3e6673ed3fc005009de952ded35546
lucas melo <lucasmeloil@pm.me>
2024-01-23 14:49:32 -0300

    Auterações finais

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 // home.component.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -12,28 +11,21 @@ export class HomeComponent {
   patientName: string = '';
   weight: number | undefined;
   height: number | undefined;
-  result: any;
-  errorMessage: string; // Deixe a inicialização para o construtor
+  errorMessage: string = '';
 
-  constructor(private router: Router, private toastr: ToastrService) {
-    this.errorMessage = ''; // Inicialize a propriedade no construtor
-  }
+  constructor(private router: Router) {}
 
   calculateIMC() {
     // Validar se os campos estão preenchidos
-    if (this.patientName && this.weight !== undefined && this.height !== undefined) {
+    if (this.patientName && this.weight && this.height) {
       // Lógica de cálculo do IMC
       const result = this.weight / (this.height * this.height);
-
-      // Exibir mensagem de sucesso
-      this.toastr.success('Cálculo do IMC realizado com sucesso!');
 
       // Redireciona para a página de resultados, passando o nome do paciente
       this.router.navigate(['/result'], { queryParams: { result, patientName: this.patientName } });
     } else {
-      // Utiliza ngx-toastr para exibir mensagem de erro
+      // Mensagem de erro para exibir na tela
       this.errorMessage = 'Por favor, preencha todos os campos.';
-      this.toastr.error(this.errorMessage, 'Erro');
     }
   }
 }
